5ab55d6993cc7aecc0560bb76e34e06d4af28bed
RDW <duckwhale@mail.ru>
2021-10-04 20:04:26 +0200

    CLI: Use arguments to run any script file passed
    It's not the world's fanciest interface, but for now it'll do.

--- main.lua
+++ main.lua
@@ -12,6 +12,18 @@ printf = require("Core/Environment/printf")
 -- LuaJIT API
 local ffi = require("ffi")
 
+function Evo:ProcessUserInput()
+	-- There's always one argument, the runtime itself (but it's stored at index "0", so Lua doesn't count it)
+	local hasCommandLineArguments = (#args > 0)
+	if not hasCommandLineArguments then
+		Evo:DisplayHelpText()
+		return
+	end
+
+	local scriptFile = args[1]
+	dofile(scriptFile) -- args is global, so there's no need to pass it along
+end
+
 function Evo:DisplayHelpText()
 	printf("Welcome to Evo.lua v%s on %s-%s!", EVO_VERSION_STRING, ffi.os, ffi.arch)
 	print()
@@ -20,4 +32,4 @@ function Evo:DisplayHelpText()
 	print("evo myScript.lua [optional command line arguments go here]")
 end
 
-Evo:DisplayHelpText()
+Evo:ProcessUserInput()
