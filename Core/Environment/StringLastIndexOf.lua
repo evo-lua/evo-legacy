b8a2deb865efa9be4d46db95b513b4d8bcdd1d08
RDW <duckwhale@mail.ru>
2021-10-09 02:15:32 +0200

    WIP: Port String.prototype.lastIndexOf() test suite from V8

--- Core/Environment/StringLastIndexOf.lua
+++ Core/Environment/StringLastIndexOf.lua
@@ -127,15 +127,27 @@ local format = string.format
 
 print("StringPrototypeLastIndexOf")
 
-for _, testCase in ipairs(testCases) do
-	local actualResult = StringPrototypeLastIndexOf(testStr, testCase.searchValue, testCase.fromIndex)
+local function assertEquals(expectedResult, actualResult, descriptor)
 
-	if actualResult ~= testCase.expectedResult then
-		print(format("FAIL\t%s IS NOT %s", actualResult, testCase.expectedResult))
+	if actualResult ~= expectedResult then
+		print(format("FAIL\t%s\t%s IS NOT %s", descriptor, actualResult, expectedResult))
 	end
 
-	assert(actualResult == testCase.expectedResult, format("\nExpected %s (actual: %s)", testCase.expectedResult, actualResult))
-	print(format("PASS\t%s IS %s", actualResult, testCase.expectedResult))
+	assert(actualResult == expectedResult, format("\nExpected %s (actual: %s)", expectedResult, actualResult))
+	print(format("PASS\t%s\t%s IS %s", descriptor, actualResult, expectedResult))
+end
+
+for index, testCase in ipairs(testCases) do
+
+	local actualResult = StringPrototypeLastIndexOf(testStr, testCase.searchValue, testCase.fromIndex)
+	assertEquals(testCase.expectedResult, actualResult, index)
+
+	-- if actualResult ~= testCase.expectedResult then
+	-- 	print(format("FAIL\t%s IS NOT %s", actualResult, testCase.expectedResult))
+	-- end
+
+	-- assert(actualResult == testCase.expectedResult, format("\nExpected %s (actual: %s)", testCase.expectedResult, actualResult))
+	-- print(format("PASS\t%s IS %s", actualResult, testCase.expectedResult))
 end
 
 print("OK\tStringPrototypeLastIndexOf")
@@ -147,3 +159,77 @@ print("OK\tStringPrototypeLastIndexOf")
 -- assert(StringPrototypeLastIndexOf(testStr, 'c', 0) == 0, "Expected 0")
 -- assert(StringPrototypeLastIndexOf(testStr, '') == 5, "Expected 5")
 -- assert(StringPrototypeLastIndexOf(testStr, '', 2) == 2, "Expected 2")
+
+-- V8 test cases below (TODO: Add licensing info)
+local s = "test test test";
+
+local MAX_DOUBLE = 1.7976931348623157e+308;
+local MIN_DOUBLE = -MAX_DOUBLE;
+local MAX_SMI = 2^30 - 1
+local MIN_SMI = - 2^30;
+local Infinity = math.huge -- #INF (infinity)
+local NaN = 0 / 0 -- #IND (indefinite)
+
+function string.lastIndexOf(...)
+	return StringPrototypeLastIndexOf(...)
+end
+
+assertEquals(10, s:lastIndexOf("test", Infinity), "tinf");
+assertEquals(10, s:lastIndexOf("test", MAX_DOUBLE), "tmaxdouble");
+assertEquals(10, s:lastIndexOf("test", MAX_SMI), "tmaxsmi");
+assertEquals(10, s:lastIndexOf("test", #s * 2), "t2length");
+assertEquals(10, s:lastIndexOf("test", 15), "t15");
+assertEquals(10, s:lastIndexOf("test", 14), "t14");
+assertEquals(10, s:lastIndexOf("test", 10), "t10");
+assertEquals(5, s:lastIndexOf("test", 9), "t9");
+assertEquals(5, s:lastIndexOf("test", 6), "t6");
+assertEquals(5, s:lastIndexOf("test", 5), "t5");
+assertEquals(0, s:lastIndexOf("test", 4), "t4");
+assertEquals(0, s:lastIndexOf("test", 0), "t0");
+assertEquals(0, s:lastIndexOf("test", -1), "t-1");
+assertEquals(0, s:lastIndexOf("test", -#s), "t-len");
+assertEquals(0, s:lastIndexOf("test", MIN_SMI), "tminsmi");
+assertEquals(0, s:lastIndexOf("test", MIN_DOUBLE), "tmindouble");
+assertEquals(0, s:lastIndexOf("test", -Infinity), "tneginf");
+assertEquals(10, s:lastIndexOf("test"), "t");
+assertEquals(-1, s:lastIndexOf("notpresent"), "n");
+assertEquals(-1, s:lastIndexOf(), "none");
+assertEquals(10, s:lastIndexOf("test", "not a number"), "nan");
+
+local longNonMatch = "overlong string that doesn't match";
+local longAlmostMatch = "test test test!";
+local longAlmostMatch2 = "!test test test";
+
+
+assertEquals(-1, s:lastIndexOf(longNonMatch), "long");
+assertEquals(-1, s:lastIndexOf(longNonMatch, 10), "longpos");
+assertEquals(-1, s:lastIndexOf(longNonMatch, NaN), "longnan");
+assertEquals(-1, s:lastIndexOf(longAlmostMatch), "tlong");
+assertEquals(-1, s:lastIndexOf(longAlmostMatch, 10), "tlongpos");
+assertEquals(-1, s:lastIndexOf(longAlmostMatch), "tlongnan");
+
+local nonInitialMatch = "est";
+
+assertEquals(-1, s:lastIndexOf(nonInitialMatch, 0), "noninit");
+assertEquals(-1, s:lastIndexOf(nonInitialMatch, -1), "noninitneg");
+assertEquals(-1, s:lastIndexOf(nonInitialMatch, MIN_SMI), "noninitminsmi");
+assertEquals(-1, s:lastIndexOf(nonInitialMatch, MIN_DOUBLE), "noninitmindbl");
+assertEquals(-1, s:lastIndexOf(nonInitialMatch, -Infinity), "noninitneginf");
+
+for i = #s + 10, 0,  -1 do
+	local expected = i < #s and i or #s;
+  assertEquals(expected, s:lastIndexOf("", i), "empty" .. i);
+end
+
+
+local reString = "asdf[a-z]+(asdf)?";
+
+assertEquals(4, reString:lastIndexOf("[a-z]+"), "r4");
+assertEquals(10, reString:lastIndexOf("(asdf)?"), "r10");
+
+-- // Should use index of 0 if provided index is negative.
+-- // See http://code.google.com/p/v8/issues/detail?id=351
+local str = "test"
+assertEquals(0, str:lastIndexOf("test", -1), "regression");
+
+print("OK\tlastIndexOf")
