d230e5798fa09922309a708b959730e0e2d3f5dd
RDW <duckwhale@mail.ru>
2021-10-09 02:15:56 +0200

    Update output format in assertion helper

--- Tests/assert.lua
+++ Tests/assert.lua
@@ -2,7 +2,7 @@ local format = string.format
 
 function _G.assertStrictEqual(actual, expected)
 	if actual ~= expected then
-		print(format("FAIL\t%s IS %s (%s)", actual, expected,  _G.currentNamespace))
+		print(format("FAIL\t%s IS NOT %s (%s)", actual, expected,  _G.currentNamespace))
 		error(format("\nExpected %s, actual: %s", expected, actual) .. "\n" .. debug.traceback())
 	end
 	print(format("PASS\t%s IS %s (%s)", actual, expected,  _G.currentNamespace))
