4e02141628533a1bdacce933b1790e8c85e9fe8e
RDW <duckwhale@mail.ru>
2021-10-10 16:30:06 +0200

    fixup String functions ported from V8

--- Core/Environment/path.lua
+++ Core/Environment/path.lua
@@ -46,18 +46,98 @@ local function StringPrototypeSlice(str, i, j)
 	return string.sub( str, i, j)
 end
 
--- If searchValue is an empty string, then fromIndex is returned.
-local function StringPrototypeLastIndexOf(str, searchValue, fromIndex)
-	if fromIndex == nil then fromIndex = math.huge end
-	-- If fromIndex < 0, the behavior will be the same as if it would be 0.
-	if fromIndex < 0 then fromIndex = 0 end
+local function StringMatchBackwards(subject, pattern, idx)
 
-	if searchValue == "" then return fromIndex end
+	local pattern_length = #pattern -- int
 
-	local returnIndex = string.find(str, searchValue, fromIndex)
-	if returnIndex == nil then returnIndex = -1 end
+	local pattern_first_char = string.sub(pattern, 1, 1)
+	for i = idx, 0, -1 do
 
-	return returnIndex
+		local characterToCheck = string.sub(subject, i + 1, i + 1)
+		if (characterToCheck == pattern_first_char) then
+			-- Check the rest of the pattern
+			local j = 1;
+			while (j < pattern_length) do
+
+				if (string.sub(pattern, j + 1, j + 1) ~= string.sub(subject, i + j + 1, i + j + 1)) then
+					break;
+				end
+
+				j = j + 1;
+			end
+
+			if (j == pattern_length) then
+				return i;
+			end
+		end
+	end
+	return -1;
+
+end
+
+local function StringLastIndexOf(sub, pat, index)
+
+	assert(sub ~= nil, "Usage: StringLastIndexOf(sub, pat, index)")
+	assert(pat ~= nil, "Usage: StringLastIndexOf(sub, pat, index)")
+	assert(index ~= nil, "Usage: StringLastIndexOf(sub, pat, index)")
+
+	sub = tostring(sub)
+	pat = tostring(pat)
+	index = tonumber(index)
+	assert(type(sub) == "string", "StringLastIndexOf: Argument sub is not a string")
+	assert(type(pat) == "string", "StringLastIndexOf: Argument pat is not a string")
+	assert(type(index) == "number", "StringLastIndexOf: Argument index is not a number")
+
+	local start_index = index
+
+	local pat_length = #pat;
+	local sub_length = #sub;
+
+	if (start_index + pat_length > sub_length) then
+	  start_index = sub_length - pat_length;
+	end
+
+	if (pat_length == 0) then
+	  return start_index
+	end
+
+	local position = -1;
+	position = StringMatchBackwards(sub, pat, start_index);
+	return position
+end
+
+local function StringPrototypeLastIndexOf(sub, pat, position)
+
+	sub = tostring(sub);
+	local subLength = #sub;
+	pat = tostring(pat);
+	local patLength = #pat;
+	local index = subLength - patLength;
+
+	local argc = 0
+	if sub ~= nil then argc = argc + 1 end
+	if pat ~= nil then argc = argc + 1 end
+	if index ~= nil then argc = argc + 1 end
+
+	if (argc > 1) then
+		position = tonumber(position);
+		if (position ~= nil) then
+			position = tonumber(position);
+			if (position < 0) then
+		  	position = 0;
+			end
+
+			if (position + patLength < subLength) then
+		 		index = position;
+			end
+		end
+	end
+
+	if (index < 0) then
+	  return -1;
+	end
+
+	return StringLastIndexOf(sub, pat, index);
 end
 
 -- -- Character codes
