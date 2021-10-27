5f1a51d718457b0f1cbe25bc56a8f23429635e16
RDW <duckwhale@mail.ru>
2021-10-27 14:20:41 +0200

    Core: Fix "attempt to yield across C-call boundary" error
    This is complimentary to the import change in evo-luvi (v2.14.2). I'm not sure if BOTH are needed, but better to be safe than sorry.

--- main.lua
+++ main.lua
@@ -14,7 +14,11 @@ function Evo:ProcessUserInput()
 	local scriptFile = args[1]
 	-- Run user scripts in a coroutine so it can yield() for sequential but non-blocking I/O
 	local runUserScript = function()
-		dofile(scriptFile) -- args is global, so there's no need to pass it along
+		-- Since user scripts will yield whenever they call into asynchronous APIs, we can't use dofile here
+		-- dofile would run the chunk in C land and cause  an "attempt to yield across C-call boundary" error
+		local compiledChunk = loadfile(scriptFile)
+		assert(compiledChunk, "Failed to load user script " .. tostring(scriptFile))
+		compiledChunk()
 	end
 
 	local userScript = coroutine.create(runUserScript)
