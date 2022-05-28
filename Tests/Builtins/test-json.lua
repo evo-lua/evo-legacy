e40c6d92f00f0eca42c877fb9712aa2f2540f2e5
RDW <duckwhale@mail.ru>
2022-05-28 02:54:52 +0200

    Tests: Update the existing JSON tests with proper indents

--- Tests/Builtins/test-json.lua
+++ Tests/Builtins/test-json.lua
@@ -6,7 +6,7 @@ assert(type(json.parse) == "function", "Should expose the parse() alias")
 
 -- Source: https://github.com/LuaDist/dkjson/pull/3 (author: Cr4xy)
 local jsonString = json.encode({A = 0, B = false, C = 0}, {keyorder = {"B", "A", "C"}})
-local expectedJsonString = '{"B":false,"A":0,"C":0}'
+local expectedJsonString = '{"B": false,"A": 0,"C": 0}'
 assert(jsonString == expectedJsonString, "Should order false values correctly when a key order is given")
 -- LPEG is a Luvi builtin, so there's really no reason to not use it
 assert(json.using_lpeg == true, "Should be using the LPEG extension to improve performance")
