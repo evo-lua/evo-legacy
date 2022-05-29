0cbf6ae443a7672d4d901c9da5ed1727bc41a0a4
RDW <duckwhale@mail.ru>
2022-05-29 14:55:25 +0200

    Update test-scenario-primitive.lua

--- Tests/Builtins/BDD/test-scenario-primitive.lua
+++ Tests/Builtins/BDD/test-scenario-primitive.lua
@@ -53,6 +53,17 @@ assertEquals(stdoutBuffer, "")
 
 scenario:Run(fauxPrint)
 
+-- Human-readable overview should indicate no handlers were registered
+local expectedOverviewText = "\n" -- To offset from the previous content (sub-optimal...)
+
+expectedOverviewText = expectedOverviewText .. "\t" -- To highlight the keywords visually
+expectedOverviewText = expectedOverviewText .. transform.cyan("Scenario: ") .. transform.white("Do nothing") .. "\n\n"
+expectedOverviewText = expectedOverviewText .. "\t" .. transform.cyan("GIVEN") .. "\t" ..  transform.white("(no preconditions)") .. "\n"
+expectedOverviewText = expectedOverviewText .. "\t" .. transform.cyan("WHEN") .. "\t" ..  transform.white("(no code to execute)") .. "\n"
+expectedOverviewText = expectedOverviewText .. "\t" .. transform.cyan("THEN") .. "\t" ..  transform.white("(no postconditions)") .. "\n"
+
+assertEquals(scenario:GetOverviewText(), expectedOverviewText)
+
 -- No assertions should be added
 local expectedResultsText = ""
 assertEquals(scenario:GetResultsText(), expectedResultsText)
