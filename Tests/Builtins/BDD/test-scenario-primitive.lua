54ae165814aedaad7168db4e2e1f4ca22d37a93a
RDW <duckwhale@mail.ru>
2022-05-29 15:24:33 +0200

    refactor testing api

--- Tests/Builtins/BDD/test-scenario-primitive.lua
+++ Tests/Builtins/BDD/test-scenario-primitive.lua
@@ -41,32 +41,6 @@ end
 
 _G.print = fauxPrint
 
--- TODO Testing, documentation, luacheckrc
-C_Testing = {}
-
-function C_Testing:CreateFauxConsole()
-
-	local tostring = tostring
-
-	local fauxConsole = {
-		stdoutBuffer = ""
-	}
-
-	function fauxConsole.print(...)
-		fauxConsole.stdoutBuffer = fauxConsole.stdoutBuffer .. tostring(... or "") .. "\n"
-	end
-
-	function fauxConsole.clear()
-		fauxConsole.stdoutBuffer = ""
-	end
-
-	function fauxConsole.read()
-		return fauxConsole.stdoutBuffer
-	end
-
-	return fauxConsole
-end
-
 ----------------------------------------------------------------------------------------------------------------
 
 import("./Scenario/test-constructor.lua")
