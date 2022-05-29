ba68ddbc58490c2557d80ccc73d0bd20b57342a5
RDW <duckwhale@mail.ru>
2022-05-29 13:17:06 +0200

    Update assertions.lua

--- Core/Builtins/assertions.lua
+++ Core/Builtins/assertions.lua
@@ -1,7 +1,17 @@
 
 -- TODO tests for this
-function assertEquals(firstValue, secondValue)
-	assert(firstValue == secondValue, "Expected " .. tostring(firstValue) .. " to be " .. tostring(secondValue))
+function assertEquals(actual, expected)
+
+	if actual == "" then actual = "<empty string>" end
+	if expected == "" then expected = "<empty string>" end
+
+	actual = transform.bold(actual)
+	expected = transform.bold(expected)
+
+	if actual ~= expected then
+		error(transform.red("\n\nASSERTION FAILURE:\n\n\t" .. tostring(actual) .. "\n\n") .. transform.red("SHOULD BE\n\n" .. tostring(expected) .. "\n"))
+	end
+	-- assert(actual == expected, "Expected " .. tostring(actual) .. " to be " .. tostring(expected))
 end
 
 _G.assertEquals = assertEquals
