91cb9a32a9fac9e2f9a692496dfd52355f3d04de
RDW <duckwhale@mail.ru>
2022-05-29 12:23:54 +0200

    test api surface is exported

--- Tests/Builtins/BDD/test-scenario-primitive.lua
+++ Tests/Builtins/BDD/test-scenario-primitive.lua
@@ -1,6 +1,26 @@
 
 local Scenario = _G.Scenario
 assert(type(Scenario) == "table", "The Scenario primitive should be exported")
+
+local exportedApiSurface = {
+	"Construct",
+	"Run",
+	"PrintResults",
+	"GetName",
+	"GetResultsText",
+	"GetNumFailedAssertions",
+	"HasFailed",
+	"GIVEN",
+	"WHEN",
+	"WHEN",
+	"THEN",
+	"FINALLY",
+}
+
+for _, fieldName in pairs(exportedApiSurface) do
+	assert(type(Scenario[fieldName]) == "function", "Should export function " .. fieldName)
+end
+
 print("OK\tBDD\t\tScenario")
 
 -- Construct
--- Tests/Builtins/BDD/test-testsuite-primitive.lua
+++ Tests/Builtins/BDD/test-testsuite-primitive.lua
@@ -1,6 +1,19 @@
 
 local TestSuite = _G.TestSuite
 assert(type(TestSuite) == "table", "The TestSuite primitive should be exported")
+
+local exportedApiSurface = {
+	"Construct",
+	"AddScenario",
+	"RunScenario",
+	"RunAllScenarios",
+	"PrintSummary",
+}
+
+for _, fieldName in pairs(exportedApiSurface) do
+	assert(type(TestSuite[fieldName]) == "function", "Should export function " .. fieldName)
+end
+
 print("OK\tBDD\t\tTestSuite")
 
 -- Construct
