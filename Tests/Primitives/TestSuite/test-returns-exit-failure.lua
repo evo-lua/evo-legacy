8978b2a9c2944da682ea0562a65f8f56d3a00c7c
RDW <duckwhale@mail.ru>
2022-06-01 15:01:53 +0200

    cleanup

--- Tests/Primitives/TestSuite/test-returns-exit-failure.lua
+++ Tests/Primitives/TestSuite/test-returns-exit-failure.lua
@@ -1,19 +1,3 @@
--- local EXIT_SUCCESS = 0
--- local EXIT_FAILURE = 1
-
--- local presumedExitCode = EXIT_SUCCESS -- Lua default, if error() was not called
-
-
-
--- local function fauxErrorHandler(...)
--- 	presumedExitCode = EXIT_FAILURE -- Whenever error() is called, the exit code is set to 1 by Lua
--- 	-- Since even the custom ERROR builtin forwards everything to the default handler in the end, this should be reliable enough
--- 	error(...)
--- end
-
--- local defaultErrorHandler = error
--- _G.error = fauxErrorHandler -- Intercept to ensure the error handler is called - we can assume it sets the exit code properly (Lua API)
-
 local testSuite = TestSuite:Construct("Failing test runner generates EXIT_FAILURE code")
 
 local failingScenario = Scenario:Construct("Scenario with failed assertion")
@@ -22,7 +6,3 @@ function failingScenario:OnEvaluate()
 end
 testSuite:AddScenario(failingScenario)
 assertFalse(testSuite:Run(), "Should return false if at least one assertion has failed")
-
--- _G.error = defaultErrorHandler -- Restore ASAP to avoid unintended side effects
-
--- -- assertEquals(presumedExitCode, EXIT_FAILURE, "Should set the exit code to EXIT_FAILURE while running a test suite if any assertions have failed")
