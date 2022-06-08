a5593d3d6eb7b61c81fc249358c13840bf17f829
RDW <duckwhale@mail.ru>
2022-06-08 04:41:28 +0200

    Update test-c-networking.lua

--- Tests/API/test-c-networking.lua
+++ Tests/API/test-c-networking.lua
@@ -1,3 +1,4 @@
 assert(type(C_Networking) == "table", "The C_Networking API should be exported")
+assert(type(Socket) == "table", "The Socket primitive should be exported")
 
 print("OK\tAPI\t\tC_FileSystem")
