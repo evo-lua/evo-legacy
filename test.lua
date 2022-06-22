1f5a3beaec1444c717f148227c18991a7170a1fa
RDW <duckwhale@mail.ru>
2022-06-22 23:20:26 +0200

    Tests: Add a basic test suite for the Networking API
    Only covers TCP functionality, since the rest doesn't have anything built on top of libuv, so there'd be no point in adding more at this time.

--- test.lua
+++ test.lua
@@ -18,6 +18,16 @@ import("Tests/Extensions/test-table.lua")
 
 import("Tests/API/test-c-filesystem.lua")
 
+local testSuites = {
+	"Tests/API/test-c-networking.lua",
+}
+
+for _, filePath in pairs(testSuites) do
+	local testSuite = import(filePath)
+	-- For CI pipelines and scripts, ensure the return code indicates EXIT_FAILURE if at least one assertion has failed
+	assert(testSuite:Run(), "Assertion failure in test suite " .. filePath)
+end
+
 import("Tests/Examples/automated-testing/run-my-tests.lua")
 import("Tests/Examples/automated-testing/faux-console-usage-example.lua")
 import("Tests/Examples/automated-testing/faux-console-injection-example.lua")
