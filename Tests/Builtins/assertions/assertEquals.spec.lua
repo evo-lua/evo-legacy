a57c5d3c73765706dacaf6021d9ccb17e741d6dd
RDW <duckwhale@mail.ru>
2022-07-23 16:50:38 +0200

    Tests: Add test cases for comparing tables with assertEquals
    Not exactly great, but it will do for now.

--- Tests/Builtins/assertions/assertEquals.spec.lua
+++ Tests/Builtins/assertions/assertEquals.spec.lua
@@ -26,4 +26,47 @@ describe("assertEquals", function()
 
 		_G.ERROR = originalErrorHandler
 	end)
+
+	it("should do nothing if comparing two identical table values", function()
+		local firstTable = {
+			hello = "world",
+			something = "else"
+		}
+		local secondTable = {
+			hello = "world",
+			something = "else"
+		}
+		assertEquals(firstTable, secondTable)
+	end)
+
+	it("should call the default ERROR handler when comparing two different table values", function()
+		local firstTable = {
+			hello = "world",
+		}
+		local secondTable = {
+			something = "different"
+		}
+
+		local originalErrorHandler = _G.ERROR
+
+		local wasErrorHandlerCalled = false
+		local lastErrorMessage = nil
+		local function fauxErrorHandler(message)
+			wasErrorHandlerCalled = true
+			lastErrorMessage = message
+		end
+
+		_G.ERROR = fauxErrorHandler
+
+		-- Need to avoid triggering errors here since we're intercepting them (awkward)
+		pcall(assertEquals, firstTable, secondTable, "Both tables should be identical")
+		assertTrue(wasErrorHandlerCalled, "Should call the default ERROR handler on assertion failure")
+
+		-- There's a bit of an assertion-ception problem here, but that's merely an inconvenience
+		local expectedText = transform.red("ASSERTION FAILURE: ") .. "Expected inputs to be equal (" .. transform.bold("world") .. " should be " .. transform.bold("nil") .. ")" .. "\n"
+
+		assertEquals(lastErrorMessage, expectedText, "Should pass an error message describing the assertion failure to the ERROR handler")
+
+		_G.ERROR = originalErrorHandler
+	end)
 end)
